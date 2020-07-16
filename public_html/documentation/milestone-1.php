0865b225dd03a50ba32289aa9694d27891a40e76
Adrian Martinez <aleemtz2@outlook.com>
2020-07-16 07:42:38 -0600

    Created directory files and deployment. Added purpose, audience, goals and Persona.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -1,7 +1,41 @@
-Purpose:
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Adrian Lee</title>
+    </head>
+    <body>
+            <header>Adrian Lee</header>
+            <h1>Purpose</h1>
+                <p>My project will help future employers better understand my skills at a glance.</p>
+            <h2>Audience</h2>
+                <p>This site is targeted at primarily at future employers. Secondarily at friends and family.</p>
+            <h3>Goal</h3>
+                <p> The primary goal of this site is to give employers a quick efficient glance at the type of programmer
+                    and person i am. </p>
 
 
-Audience
+            <h4>Persona<h4>
+               <ol>
+                    <li>Name: Jeff Boss
+                    <li>Age: 35</li>
+                    <li>Business Owner</li>
+                    <li>Software engineer</li>
+                    <li>10 + years in the field</li>
+                    <li>Over worked</li>
+                    <li>Constantly looking to add talent to his team</li>
+                </ol>
+            <h5>Devices</h5>
+                <ol>
+                    <li>Phone:  Iphone 11</li>
+                    <li>Tablet: iPad mini</li>
+                    <li>Computer: Mac Book pro</li>
+                    <li>OS: Mac OS X</li>
+                    <li>Browser: Safari </li>
+                </ol>
 
+            <h6>User Story</h6>
+                <p>As a Lead developer Jeff wants to build a pro team around him quickly.
 
-Goal
+    </body>1
+</html>
